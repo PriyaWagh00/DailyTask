fb6141f85407c9ffca006362001cf5eaa40e8ac2
PriyaWagh00 <priyarwagh00@gmail.com>
2025-03-22 23:27:13 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+//import java.util.
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int i, j;
+        for (i = 0; i <= 5; i++) {
+            for (j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
